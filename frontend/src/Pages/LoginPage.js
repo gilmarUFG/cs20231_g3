4b8caf9ce4e090bf3a38b37ab874879b56f534ae
Murilo <mufreua10@gmail.com>
2023-05-13 15:31:32 -0300

    Mudanças no frontend do sistema.

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,6 +1,7 @@
 import {useContext, useState} from "react";
 import {Navigate} from "react-router-dom";
 import {UserContext} from "../UserContext";
+import { Button, FormControl, FormLabel, Input, Text } from "@chakra-ui/react";
 
 export default function LoginPage() {
 
@@ -34,11 +35,18 @@ export default function LoginPage() {
     }
     
     return (
-        <form className="login" onSubmit={login}>
-            <h1>Login</h1>
-            <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}/>
-            <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
-            <button type="submit">Login</button>
-        </form>
+        <FormControl className="form" onSubmit={login}>
+            <Text style={{ fontSize:"25px" }}><strong>Login</strong></Text>
+            <br></br>
+
+            <FormLabel className="form-label">Usuário:</FormLabel>
+            <Input width="300px" type="text" value={username} onChange={ev => setUsername(ev.target.value)}/>
+
+            <FormLabel className="form-label">Senha:</FormLabel>
+            <Input width="300px" type="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
+
+            <br></br>
+            <Button marginTop={"30px"} colorScheme="purple">Entrar</Button>
+        </FormControl>
     )
 }
