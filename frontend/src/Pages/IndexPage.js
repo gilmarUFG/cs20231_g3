8242c15a5f65a1ec557a99bd88ae775cf0bb3b2f
Kaua J <kauajuno@gmail.com>
2023-05-31 20:21:05 -0300

    Update do layout da navbar e das cores

--- frontend/src/Pages/IndexPage.js
+++ frontend/src/Pages/IndexPage.js
@@ -1,9 +1,10 @@
-import Project from "../Components/Project";
+import Project from "../Components/Project/Project.js";
 import App from '../App';
 
 export default function IndexPage() {
     return (
         <div className="projetos">
+            <Project/>
             <Project/>
             <Project/>
         </div>
